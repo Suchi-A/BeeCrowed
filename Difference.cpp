23eaca741e95154894e023f86b6be2db785d8b97
Humaira Akter Suchi <186734076+Suchi-A@users.noreply.github.com>
2026-08-22 23:17:45 +0600

    Add files via upload

---
+++ Difference.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int A;
+    int B;
+    int C;
+    int D;
+    scanf("%d %d %d %d",&A,&B,&C,&D);
+    int X=A*B-C*D;
+    printf("DIFERENCA = %d\n",X);
+    return 0;
+
+}
